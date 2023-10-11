ed7db3ce47953849cb0dd91ee75dbde0e6594f6e
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-10-11 09:58:32 -0400

    Added google operator

--- dags/synapse_dag.py
+++ dags/synapse_dag.py
@@ -1,12 +1,30 @@
-from datetime import datetime
+import datetime
 from airflow import DAG
+# from operators.RunSynapsePipelineOperator import AzureSynapseRunPipelineOperator
+from operators.googleOperator import MyFirstOperator
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="airflow-ci-cd-tutorial",
-    start_date=datetime(2023, 8, 15),
-    schedule="0 0 * * *",
-    tags=["tutorial", "CI/CD"]
+    dag_id="AzureSynapseRunPipelineDag",
+    schedule=None,
+    start_date=datetime.datetime(2021, 1, 1),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["pipeline"],
 ) as dag:
-    python_version = BashOperator(task_id="pythonversion", bash_command="echo HELLO")
-    python_version
+    # trigger_synapse_pipeline = AzureSynapseRunPipelineOperator(
+    #     azure_synapse_conn_id="azure_synapse_connection",
+    #     task_id="trigger_synapse_pipeline",
+    #     pipeline_name="Pipeline 1",
+    #     azure_synapse_workspace_dev_endpoint="https://ambika-synapse-workspace.dev.azuresynapse.net",
+    #     deferrable=False 
+    # )
+
+    trigger_google_operator = MyFirstOperator(
+        task_id="Google_operator"
+    )
+
+    python_version = BashOperator(task_id="pythonversion", bash_command="python --version")
+
+    trigger_google_operator >> python_version  
+    
