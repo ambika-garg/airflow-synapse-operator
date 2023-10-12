b40c58f0fef6d4a97ba78dd5f1fe88696d7d5db8
ambika-garg <70703123+ambika-garg@users.noreply.github.com>
2023-10-12 16:13:47 -0400

    synapse extra operator link

--- plugins/synapseExtraOperatorLink.py
+++ plugins/synapseExtraOperatorLink.py
@@ -2,8 +2,8 @@ from airflow.plugins_manager import AirflowPlugin
 from operators.RunSynapsePipelineOperator import AzureSynapsePipelineRunLink
 
 
-class AirflowExtraLinkPlugin(AirflowPlugin):
-    name = "extra_link_plugin"
+class SynapseExtraLinkPlugin(AirflowPlugin):
+    name = "synapse_link_plugin"
     operator_extra_links = [
         AzureSynapsePipelineRunLink(),
     ]
